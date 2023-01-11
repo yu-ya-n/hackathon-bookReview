71a9c6043ea84bf9cc2b089cfc4a8ee94b896b4d
y_tanaka <yuya_tanaka@arena-pro.co.jp>
2023-01-11 09:11:48 +0900

    frontendのlint設定

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@nuxtjs/eslint-config-typescript',
+    '@vue/prettier'
+  ],
+  overrides: [],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {}
+}
